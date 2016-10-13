5273028af7fff4f2f864d769cc24146e07ffd26d
Shashank-Mahesh <shash.v.mahesh@gmail.com>
2016-10-13 16:19:25 -0700

    created file
    autonomous version 1

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousVersionOne.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous (name="Concept: Auto v1", group = "Concept")
+public class AutonomousVersionOne extends LinearOpMode{
+
+    DcMotor motorLeft, motorRight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
+
+}
